fae60e5c8803e545c497fb02f971d0b1bda02799
NoFishLikeIan <andreatitton96@gmail.com>
2022-03-10 14:44:09 +0100

    🦉 fixed model definition

--- src/definitions.jl
+++ src/definitions.jl
@@ -2,9 +2,11 @@ Goods = Vector{Vector{Int64}}
 
 mutable struct Firm <: AbstractAgent
     id::Int64
-    x::Vector{Int64}
-    isfunctional::Bool
-    μ::Float64
+    isfunctional::Bool # i ∈ F
+
+    x::Vector{Int64} # Suppliers
+    b::Matrix{Float64} # Perceived probability over neighbors
+    μ::Float64 # Idiosyncratic risk
 end
 
 function good(f::Firm, G::Goods)
