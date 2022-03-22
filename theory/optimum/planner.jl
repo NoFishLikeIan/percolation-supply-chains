ca2488fb73aff27c4daa441261afc384070f404e
NoFishLikeIan <andreatitton96@gmail.com>
2022-03-22 16:22:46 +0100

    🐛 bug fix in probability definition

--- theory/optimum/planner.jl
+++ theory/optimum/planner.jl
@@ -1,20 +1,22 @@
 function p(i::Int64, S::Vector{Float64}; m::VerticalModel)
     if i == 0 return 1. end
 
-    risk = 1 - p(i - 1, S; m)
-    return (1 - m.μ[i]) * (1 - risk)^S[i]
+    supres = 1 - p(i - 1, S; m)
+    ownrisk = 1 - m.μ[i]
+
+    return ownrisk * (1 - supres^S[i])
 end
 
 function ∂p(i, j, S; m::VerticalModel)
     if i < j || j == 1 return 0. end
 
     ownrisk = 1 - m.μ[i]
-    uprisk = 1 - p(i - 1, S; m)
+    supres = 1 - p(i - 1, S; m)
 
     if i == j
-        return -ownrisk * uprisk^S[i] * log(uprisk)
+        return -ownrisk * supres^S[i] * log(supres)
     elseif i > j
-        return ownrisk * S[i] * uprisk^S[i - 1] * ∂p(i - 1, j, S; m)
+        return ownrisk * S[i] * supres^S[i - 1] * ∂p(i - 1, j, S; m)
     end
 end
 
@@ -29,7 +31,7 @@ end
 
 function focfactory(m::VerticalModel)
     function foc!(F, Sᵣ)
-        S = [0., Sᵣ...] # Dummy for S₀
+        S = [1., Sᵣ...] # Dummy for S₀
 
         n = length(S)
 
