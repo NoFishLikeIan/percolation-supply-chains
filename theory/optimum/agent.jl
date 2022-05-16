13244faced31539f97992012f83d8434d2bbf25a
NoFishLikeIan <andreatitton96@gmail.com>
2022-05-16 16:22:22 +0200

    🪐 higher order approximation

--- theory/optimum/agent.jl
+++ theory/optimum/agent.jl
@@ -44,22 +44,31 @@ function Vp(s::Real, Fₛ::FuncDistribution; model::VerticalModel)
     return V(Fₛ, f)
 end
 
+"""
+Using Sterling approximation of factorial
+"""
+function p̃(s::Real, v::Real; model::VerticalModel)
+    m = model.m
+    Δₛ(x) = x * log((x - s) / x) - s * log(x - s)
+
+    1 - exp(Δₛ(m) - Δₛ(m - v))
+end
 
 function p(s::Real, v::Real; model::VerticalModel)
-    if s == 0
-        return 0.
-    end
-
     m = model.m
+    
+    nodiv = s == 0
+    fulldiv = s > 1 + m - v
+    largem = m > 100
 
-    if s > 1 + m - v
-        return 1.
-    else
-        constant = gamma(1 + m - s) / gamma(1 + m)
-        random = gamma(1 + m - s - v)  / gamma(1 + m - v)
+    if nodiv return 0. end
+    if fulldiv return 1. end
+    if largem return p̃(s, v; model) end
 
-        return 1. - constant / random
-    end    
+    constant = gamma(1 + m - s) / gamma(1 + m)
+    random = gamma(1 + m - s - v)  / gamma(1 + m - v)
+
+    return 1. - constant / random    
 end
 
 function ∂p(s::Real, v::Real; model::VerticalModel)
@@ -68,6 +77,27 @@ function ∂p(s::Real, v::Real; model::VerticalModel)
     return (1 - p(s, v; model)) * Δψ₀
 end
 
+function ∂²p(s::Real, v::Real; model::VerticalModel)
+    m = model.m
+
+    Δψ₀ = ψ₀(1 + m - v) - ψ₀(1 + m - v - s)
+    Δψ₁ = ψ₁(1 + m - v) - ψ₁(1 + m - v - s)
+    return -(1 - p(s, v; model)) * (Δψ₀^2 + Δψ₁)
+end
+
+function ∂³p(s::Real, v::Real; model::VerticalModel)
+    m = model.m
+
+    Δψ₀ = ψ₀(1 + m - v) - ψ₀(1 + m - v - s)
+    Δψ₁ = ψ₁(1 + m - v) - ψ₁(1 + m - v - s)
+    Δψ₂ = ψ₂(1 + m - v) - ψ₂(1 + m - v - s)
+
+    pₖ = p(s, v; model)
+    pₖ′ = ∂p(s, v; model)
+    
+
+    return pₖ′ * (Δψ₀^2 + Δψ₁) + (1 - pₖ) * (2Δψ₀ * Δψ₁ + Δψ₂)
+end
 
 function compequilibrium(model::VerticalModel; integer = false)
     F₀ = Binomial(model.m, 1 - model.μ₀)
--- theory/utils/lawofmotion.jl
+++ theory/utils/lawofmotion.jl
@@ -1,5 +1,7 @@
 ψ₀(x) = polygamma(0, x)
 ψ₁(x) = polygamma(1, x)
+ψ₂(x) = polygamma(2, x)
+
 
 function G₁(x; sₖ)
     f, ρ = x
@@ -16,16 +18,23 @@ end
 function G(x; sₖ, model::VerticalModel)
     f, ρ = x
     m = model.m
+
+    VF = m * (1 - f) * f * (1 + (m - 1) * ρ)
+    SF = VF * (1 - 2f) * (1 + (2m - 1) * ρ) / (1 + ρ)
+
+	NKF = ρ^2 * (6*(m-1)*m + 2 - 3f*(7*(m-1)*m + 2)*(1-f)) + 3*(1-f)*f*(((m-8)*m + 4)*ρ + m - 2) + 6m*ρ - 3ρ + 1
+	DKF = (ρ + 1) * (2ρ + 1)
+	KF = VF * NKF / DKF
     
     fⁿ = G₁(x; sₖ)
-    D = fⁿ * (1 - fⁿ)
-    N = ∂p(sₖ, m * f; model)^2 * m * (1 - f) * f * (1 + (m - 1) * ρ) 
 
-    ρⁿ = N / D
+    order2 = ∂p(sₖ, m * f; model)^2 * VF
+    order3 = ∂p(sₖ, m * f; model) * ∂²p(sₖ, m * f; model) * SF
+    order4 = (∂²p(sₖ, m * f; model)^2 / 4 + (2 / 9) * ∂p(sₖ, m * f; model) * ∂³p(sₖ, m * f; model)) * KF
 
-    ρⁿ = isnan(ρⁿ) ? 0 : ρⁿ
+    ρⁿ = (order2 + order3 + order4) / (fⁿ * (1 - fⁿ))
 
-    return [fⁿ, ρⁿ]
+    return clamp.([fⁿ, ρⁿ], 0, 1)
 end
 
 
