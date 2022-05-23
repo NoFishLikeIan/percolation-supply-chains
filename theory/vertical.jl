79fb76f27a8e5f2213c5bfdb230301e764f39243
NoFishLikeIan <andreatitton96@gmail.com>
2022-05-23 16:14:01 +0200

    new coloring of plot

--- theory/vertical.jl
+++ theory/vertical.jl
@@ -15,7 +15,7 @@ begin
 end
 
 begin
-    using Plots, LaTeXStrings
+    using Plots, LaTeXStrings, Colors
     theme(:dao); default(size = 500 .* (√2, 1))
     plotpath = get(ENV, "PLOT_PATH", "")
 end
@@ -25,6 +25,7 @@ include("utils/probability.jl")
 include("utils/dynamicalsystem.jl")
 include("utils/distcompound.jl")
 include("utils/lawofmotion.jl")
+include("utils/taylorapproximation.jl")
 
 include("optimum/planner.jl")
 include("optimum/agent.jl")
@@ -34,16 +35,20 @@ include("simulate.jl")
 
 # Assume that m is constant and μᵢ > 0 only for i = 0.
 
-K = 20
-m = 100
-μ₀ = 0.01
-profit = 100.
+K = 2 # Length economy
+m = 100 # Size nodes
+μ₀ = 0.01 # Basal risk
+r = 0.01 # cost / profit ratio
 
-model = VerticalModel(m, μ₀, 1 / profit, K)
-Fₛ, sₛ = compequilibrium(model)
+model = VerticalModel(m, μ₀, r, K)
 
-x = [0.99, 0.4]
+fᵣ = 0.9
+sᵣ = 1.2
 
-sspace = range(1., 2.; length = 101)
-plot(sspace, s -> G₂(x; sₖ = s, model), label = nothing, c =:blue)
-plot!(twinx(), sspace, s -> G₁(x; sₖ = s), label = nothing, c =:red)
+ρspace = 0:0.01:0.2
+sspace = range(0.05, 2.5; length = 101)
+vspace = range(0, m - 1; length = 101)
+
+Eφ(ρ) = φ̃(m, sᵣ, fᵣ, ρ) / φ(m, sᵣ, 0)
+
+plot(ρspace, Eφ)
