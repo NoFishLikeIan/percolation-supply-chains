097e1e9200d1a754e31b300d60e31c0ee148c29d
NoFishLikeIan <andreatitton96@gmail.com>
2022-07-04 20:47:58 +0200

    🪐 new planner solutin attempt

--- theory/optimum/planner.jl
+++ theory/optimum/planner.jl
@@ -10,3 +10,59 @@ function plannerequilibrium(model::VerticalModel)
         return sₖ
     end
 end
+
+function plannersymmetricsolution(model::VerticalModel; L=20, verbose=false)
+    m, r = model.m, model.r
+    fspace = range(0.01, 0.99; length=L)
+    ρspace = copy(fspace)
+
+    sspace = range(0, m; length=L)
+
+    sₖ = Array{Float64}(undef, (L, L, model.K))
+    Vₖ = copy(sₖ)
+
+    indices = product(1:L, 1:L) |> collect
+
+    for k ∈ reverse(1:K)
+        verbose && print("Planner: solving layer $k / $K...\r")
+
+        Threads.@threads for (i, j) ∈ indices
+            f, ρ = fspace[i], ρspace[j]
+
+            if k < K
+
+                Jₖᵢⱼ = Vector{Float64}(undef, length(sspace))
+                for (l, s) ∈ enumerate(sspace)
+                    f′, ρ′ = G([f, ρ]; sₖ=s)
+                    itp = LinearInterpolation(
+                        (fspace, ρspace), Vₖ[:, :, k+1];
+                        extrapolation_bc=Line()
+                    )
+
+                    Jₖᵢⱼ[l] = model.m * (f′ - s * model.r) + itp(f′, ρ′)
+                end
+
+                idxmax = argmax(Jₖᵢⱼ)
+                sₖ[i, j, k] = sspace[idxmax]
+                Vₖ[i, j, k] = Jₖᵢⱼ[idxmax]
+
+            else
+                s = agentoptimum(f, ρ; m, r)
+                f′ = G₁([f, ρ]; sₖ=s)
+
+                sₖ[i, j, end] = s
+                Vₖ[i, j, end] = model.m * (inv(model.r) * f′ - s)
+
+            end
+        end
+    end
+
+    function planneroptimum(f, ρ, k::Int64)
+        LinearInterpolation(
+            (fspace, ρspace), sₖ[:, :, k];
+            extrapolation_bc=Line()
+        )(f, ρ)
+    end
+
+    return planneroptimum
+end
--- theory/vertical.jl
+++ theory/vertical.jl
@@ -1,5 +1,3 @@
-using DotEnv; DotEnv.config()
-
 using SpecialFunctions
 using IterTools, Combinatorics
 
@@ -8,23 +6,30 @@ using ForwardDiff
 using LinearAlgebra
 using Interpolations
 
+using Base.Threads
+
 using DynamicalSystems
 
 begin
     using Random
     seed = parse(Int64, get(ENV, "SEED", "123"))
     Random.seed!(seed)
-    
+
     using StatsBase, Distributions
 end
 
 begin
     using Plots, LaTeXStrings, Colors
-    theme(:dao); default(size = 500 .* (√2, 1))
+    theme(:dao)
+    default(size=500 .* (√2, 1))
     plotpath = get(ENV, "PLOT_PATH", "")
 end
 
+using DotEnv
+DotEnv.config()
+
 include("definitions.jl")
+
 include("utils/probability.jl")
 include("utils/dynamicalsystem.jl")
 include("utils/distcompound.jl")
@@ -39,6 +44,8 @@ include("optimum/correlation.jl")
 K = 20 # Length economy
 m = 100 # Size nodes
 μ₀ = 0.01 # Basal risk
-r = 0.02 # cost / profit ratio
+r = 0.1 # cost / profit ratio
 
 model = VerticalModel(m, μ₀, r, K)
+@btime plannersymmetricsolution(model; L=50)
+@btime plannersymmetricsolution(model; L=100)
