75e9d53ef751a16ade2069c90ac06e865e6c37b4
NoFishLikeIan <andreatitton96@gmail.com>
2022-07-07 16:21:31 +0200

    🦉 correct planner problem

--- theory/optimum/planner.jl
+++ theory/optimum/planner.jl
@@ -1,68 +1,22 @@
-function plannerequilibrium(model::VerticalModel)
-    r, μ₀ = model.r, model.μ₀
-    m, K = model.m, model.K
+function planneroptimum(m::Int64, r::Float64, μ₀::Float64, k::Int64)
+    n = (log(r) - (log ∘ log)(1/μ₀)) / log(μ₀)
 
-    if r > (1 - μ₀^m)
-        return zeros(K)
-    else
-        sₖ = ones(K)
-        sₖ[1] = m
-        return sₖ
-    end
+    if n < 0 return 0 end
+    if k > 1 return 1 end
+
+    return min(n, m)
 end
 
-function plannersymmetricsolution(model::VerticalModel; L=20, verbose=false)
-    m, r = model.m, model.r
-    fspace = range(0.01, 0.99; length=L)
-    ρspace = copy(fspace)
+"""
+G with planner's sₖ = s̃ₚ
+"""
+function Gₚ(x, m::Int64, r::Float64, μ₀::Float64, k::Int64)
+    sₖ = planneroptimum(m, r, μ₀, k)
+    return G(x; sₖ)
+end
+Gₚ(x, model::VerticalModel, k::Int64) = Gₚ(x, model.m, model.r, model.μ₀, k)
 
-    sspace = range(0, m; length=L)
-
-    sₖ = Array{Float64}(undef, (L, L, model.K))
-    Vₖ = copy(sₖ)
-
-    indices = product(1:L, 1:L) |> collect
-
-    for k ∈ reverse(1:K)
-        verbose && print("Planner: solving layer $k / $K...\r")
-
-        Threads.@threads for (i, j) ∈ indices
-            f, ρ = fspace[i], ρspace[j]
-
-            if k < K
-
-                Jₖᵢⱼ = Vector{Float64}(undef, length(sspace))
-                for (l, s) ∈ enumerate(sspace)
-                    f′, ρ′ = G([f, ρ]; sₖ=s)
-                    itp = LinearInterpolation(
-                        (fspace, ρspace), Vₖ[:, :, k+1];
-                        extrapolation_bc=Line()
-                    )
-
-                    Jₖᵢⱼ[l] = model.m * (f′ - s * model.r) + itp(f′, ρ′)
-                end
-
-                idxmax = argmax(Jₖᵢⱼ)
-                sₖ[i, j, k] = sspace[idxmax]
-                Vₖ[i, j, k] = Jₖᵢⱼ[idxmax]
-
-            else
-                s = agentoptimum(f, ρ; m, r)
-                f′ = G₁([f, ρ]; sₖ=s)
-
-                sₖ[i, j, end] = s
-                Vₖ[i, j, end] = model.m * (inv(model.r) * f′ - s)
-
-            end
-        end
-    end
-
-    function planneroptimum(f, ρ, k::Int64)
-        LinearInterpolation(
-            (fspace, ρspace), sₖ[:, :, k];
-            extrapolation_bc=Line()
-        )(f, ρ)
-    end
-
-    return planneroptimum
+function JGₚ(x, m::Int64, r::Float64, μ₀::Float64, k::Int64)
+    sₖ = planneroptimum(m, r, μ₀, k)
+    return JG(x; sₖ)
 end
